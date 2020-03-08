9456e7cbd1f6b257e937c35f5267fdf9771a386c
我鈤你嘴2 <835670008@qq.com>
2020-03-09 01:31:18 +0800

    Site updated: 2020-03-09 01:31:10

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+        {
+            name: "with you",
+            artist: 'ずっとすきして たくさんすきして',
+            url: 'images/with you.mp3',
+            cover: 'https://i.loli.net/2020/03/09/QDqxvoaS1tArKXG.png',
+        }
+    ]
+});
